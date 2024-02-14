f8929c1cf8f868656f356a02ba34a46e64a7147e
студент НФ УУНиТ <@nfbashgu.local>
2024-02-14 09:14:40 +0500

    Lesson 01

---
+++ 01/01.py
@@ -0,0 +1,11 @@
+t = int(input('Enter the temperature: '))
+if t >40:
+    print('Горячо')
+elif t > 20:
+    print('Тепло')
+elif t > 0:
+    print('Прохладно')
+elif t > -10:
+    print('Холодно')
+else:
+    print('Дубак')
