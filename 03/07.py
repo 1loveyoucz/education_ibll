221b2b06e4047e5c4f12dfcb243d146b4417040b
1loveyoucz <159651395+1loveyoucz@users.noreply.github.com>
2024-02-21 17:27:24 +0300

    Update 07.py

--- 03/07.py
+++ 03/07.py
@@ -1,10 +1,18 @@
-minn = 0
-maxx = 1001
-answ = input()
-th = 0
-while answ != '=':
-    th = (maxx + minn) // 2
-    if answ == '>':
-        th == maxx
-print()
-
+print('Загадайте число от 1 до 1000')
+minn = 1
+maxx = 1000
+attempts = 0
+while minn <= maxx and attempts < 10:
+    hidden_number = (minn + maxx) // 2
+    print(hidden_number)
+    answer = input('Введите символ:\n<\n>\n=\n')
+    if answer == '<':
+        maxx = hidden_number - 1
+    elif answer == '>':
+        minn = hidden_number + 1
+    elif answer == '=':
+        print(f'Ваше число: {hidden_number}')
+        break
+    attempts += 1
+else:
+    print('Закончились попытки')
