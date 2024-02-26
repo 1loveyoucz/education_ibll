b973a07f65737ab9c76c0792c3a0574242bc419f
1loveyoucz <159651395+1loveyoucz@users.noreply.github.com>
2024-02-26 17:58:51 +0300

    Update 01.py

--- 04/01.py
+++ 04/01.py
@@ -1,9 +1,11 @@
-temperature = []
 temperature_1 = float(input())
+temperature = []
 while temperature_1 < 22.0:
     temperature += [temperature_1]
     temperature_1 = float(input())
-d = 0
-for d in len(temperature):
-    d += 1
-    print(float(d))
+
+if len(temperature) >= 7:
+    weeks = len(temperature) // 7
+    print(f'Количество недель: {weeks}')
+else:
+    print(0)
