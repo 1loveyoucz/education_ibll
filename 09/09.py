32751b3e622ed7200dec0668a2944ad4638711c5
Ilmir Mullayarov <m.few@bk.ru>
2024-03-15 18:40:17 +0300

    Lesson 09

---
+++ 09/09.py
@@ -0,0 +1,15 @@
+people = int(input('Кол-во людей: '))
+n_l = []
+for i in range(people):
+    n = int(input(''))
+    n_l += [n]
+for n in n_l:
+    print(n)
+print()
+for n in n_l:
+    print(n * 3)
+
+
+
+
+
